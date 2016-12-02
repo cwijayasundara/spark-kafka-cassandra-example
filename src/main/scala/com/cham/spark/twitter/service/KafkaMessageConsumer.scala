16b6bb45aad20e8f9f1b165c10c09baba3a19e81
cwijayasundara <cwijayasundara@gmail.com>
2016-12-02 15:43:31 +0000

    Added the first version of the Kafka message consumer

---
+++ src/main/scala/com/cham/spark/twitter/service/KafkaMessageConsumer.scala
@@ -0,0 +1,69 @@
+package com.cham.spark.twitter.service
+
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import java.util.Properties
+import java.util.Collections
+
+import com.google.gson.Gson
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Consumes messages off Kafka streams
+  *
+  *
+  *
+  * Created by cwijayasundara on 01/12/2016.
+  */
+
+object KafkaMessageConsumer {
+
+    def main(args: Array[String]) {
+
+      val sparkConf = new SparkConf().setAppName("spark-consumer").setMaster("local[4]")
+      val ssc = new StreamingContext(sparkConf, Seconds(2))
+
+      val kafkaParams = Map[String, Object](
+        "bootstrap.servers" -> "localhost:9092",
+        "key.deserializer" -> classOf[StringDeserializer],
+        "value.deserializer" -> classOf[StringDeserializer],
+        "group.id" -> "group1",
+        "auto.offset.reset" -> "latest",
+        "enable.auto.commit" -> (false: java.lang.Boolean)
+      )
+
+      val topics = Array("poc")
+      val stream = KafkaUtils.createDirectStream[String, String](
+        ssc,
+        PreferConsistent,
+        Subscribe[String, String](topics, kafkaParams)
+      )
+
+      stream.map(new Gson().toJson(_)).print()
+
+      ssc.start()
+      ssc.awaitTermination()
+    }
+
+  /*
+   * Method that use the direct kafka consumer API
+   */
+
+  def consumeFromDirectKafkaApi(): Unit ={
+
+    val kafkaProps: Properties = new Properties
+    kafkaProps.put("bootstrap.servers", "localhost:9092")
+    kafkaProps.put("group.id", "poc")
+    kafkaProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    kafkaProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+
+    val kafkaConsumer = new KafkaConsumer[String, String](kafkaProps)
+    kafkaConsumer.subscribe(Collections.singletonList("poc"))
+    val numberOfMessages = kafkaConsumer.poll(100).count()
+    println("Number of messages in the Kafka topic is " + numberOfMessages)
+  }
+}
